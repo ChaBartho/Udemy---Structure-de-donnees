b5f11d6e86a53728db0b766233055c367de3b9d2
Charlotte <you@example.com>
2023-01-22 12:27:47 +0100

    Section 9 - derniere leçon

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
 
 //TABLEAU 1 DIMENSION
+        System.out.println("------------------TAB 1D : ----------------");
         int[] tableauDeInt = new int[5];
         char[] tableauDeChar = new char[5];
         String[] tableauDeString = new String[5];
@@ -24,6 +24,7 @@ public class Main {
 
 
 //TABLEAU 2 DIMENSIONS
+        System.out.println("------------------TAB 2D : ----------------");
         int[][] tableauDeInt2D = new int [3][3];    //3 colonnes et 3 lignes
         tableauDeInt2D [0][0] = 1;
         tableauDeInt2D [0][1] = 2;
@@ -45,7 +46,12 @@ public class Main {
 
 
 //LIST
+//ordre en fonction des ajouts
+        System.out.println("-------------------LIST : -----------------");
         Voiture peugeot206 = new Voiture();
+        peugeot206.couleur="rouge";
+        Voiture peugeot207 = new Voiture();
+        peugeot207.couleur="noire";
 
         List list = new ArrayList();
         list.add(peugeot206);  //index 0   -> SUPPRIMÉ (methode remove)
@@ -59,17 +65,113 @@ public class Main {
         //Si je ne peux pas anticiper le type de l'élément récupéré, je fais une var de type Object pour récupérer cet élément
 
         System.out.println(obj);
+        System.out.println(list.size());    // = 3
 
 
 
-//SET
+//SET = type de Collection qui peut contenir un ensemble non ordonné d'Objets mais qui n'accepte pas les doublons.
+//ordre et position des éléments totalement imprévisibles
+        System.out.println("-------------------SET : -------------------");
 
-//FOREACH
+        Set<Voiture> set = new HashSet<>();
+        set.add(peugeot206);
+        set.add(peugeot207);
+        set.add(peugeot206);    // -> Je ne peux pas ajouter 2x le même élément dans une HashSet, celui-ci a été ignoré
 
-//MAP
+        System.out.println(set.size()); // = 2
 
 
+//FOREACH = itérer/parcourir sur une collection
+//( élément1  :  élément2 ) -> élément1 = la variable qui va contenir successivement chacun des éléments de la collection lors de son parcours / élément2 = la collection sur laquelle je souhaite itérer
+        System.out.println("-------------------FOREACH : -------------------");
 
+        for (Voiture voiture : set) {
+            System.out.println(voiture.couleur);
+        }
+
+        Iterator<Voiture> it =  set.iterator();
+
+        while( it.hasNext() ) { // Renvoie vrai tant qu'il existe un prochain élément dans la collection
+
+            Voiture v = it.next();
+            System.out.println(v.couleur);
+        }
+
+
+//MAP = référence chaque élément grâce à une clé    -> On peut insérer 2x la même valeur, mais pas 2x la même clé
+        System.out.println("-------------------MAP : -------------------");
+
+        Voiture peugeot205 = new Voiture();
+        peugeot205.couleur="bleue";
+
+        Map<String, Voiture> map = new HashMap();
+        map.put("205", peugeot205);
+        map.put("207", peugeot207);
+
+        Voiture v = map.get("207");
+
+        System.out.println(v.couleur);
+
+        //parcourir une map = foreach   -> à gauche des :  = la variable qui va stocker chacun des éléments de la map (sauf que les éléments sont matérialisés par une clef ET une valeur = couple d'objet -> objet qui va contenir un couple d'objet = Entry = classe interne de Map)
+        //Si je veux savoir l'ensemble des couple <K,V>
+        for (Map.Entry<String, Voiture> entry : map.entrySet()) {
+            String key = entry.getKey();
+            Voiture value = entry.getValue();
+            System.out.println("l'élément suivant a pour clef : "+key+" il s'agit d'une voiture de couleur "+value.couleur);
+        }
+
+        //Si je veux juste savoir les K     keySet() renvoie un set -> clef = unique
+        for (String key : map.keySet()) {
+            System.out.println("l'élément suivant a pour clef : "+key);
+        }
+        //Si je vuex savoir l'ensemble des V        values() renvoie une liste -> doublons possibles
+        for (Voiture value : map.values()) {
+            System.out.println("il s'agit d'une voiture de couleur "+value.couleur);
+        }
+
+
+        System.out.println("le nombre d'éléments dans la map est de : "+map.size());
+        System.out.println("la map est-elle vide ? "+map.isEmpty());
+        System.out.println("la map contient-elle une 207 ? "+map.containsKey("207"));
+        System.out.println("la map contient-elle la peugeot 207 ? "+map.containsValue(peugeot207));
+
+        //map.clear(); //Vider la collection
+
+        //Collection = interface
+        //Collections = classe utilitaire de java avec méthodes static(+-50)
+        //Collections.replaceAll(DansTelleListe, argumentARemplacer, argumentDeRemplacement)
+
+
+//SORTEDSET & SORTEDMAP
+        System.out.println("-------------------TREESET : -------------------");
+
+        Set<Carre> setCarre = new TreeSet<>();
+        setCarre.add(new Carre(10l));
+        setCarre.add(new Carre(8l));
+        setCarre.add(new Carre(50l));
+        setCarre.add(new Carre(2l));
+
+        for (Carre carre : setCarre){
+            System.out.println(carre.cote);
+        }
+        System.out.println("-------------------TREEMAP : -------------------");
+
+        Map<Carre, Voiture> mapCarre = new TreeMap<>();
+        mapCarre.put(new Carre(10l), peugeot205);
+        mapCarre.put(new Carre(8l), peugeot206);
+        mapCarre.put(new Carre(50l), peugeot207);
+        mapCarre.put(new Carre(2l), peugeot207);
+
+        for (Map.Entry<Carre, Voiture> entry : mapCarre.entrySet()){
+            System.out.println(entry.getKey().cote+" "+entry.getValue().couleur);
+        }
+
+//COMPARABLE
+
+        //Qu'est ce que l'ordre naturel dans une collection d'objets ?
+        //Les objets d'une List par exemple peuvent être ordonnées en invoquant la méthode Collections.sort.
+        // Mais c'est au développeur de définir comment les objets doivent se comparer entre eux
+        // et ceci est défini dans la méthode compareTo issue de l'interface Comparable.
 
 
 
